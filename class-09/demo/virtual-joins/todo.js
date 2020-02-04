430170fb5f8e6146409d5b7200b192b1b1c791b9
sohad qtaitat <38030859+sohadQtaitat@users.noreply.github.com>
2020-02-05 00:51:58 +0200

    Update todo.js

--- class-09/demo/virtual-joins/todo.js
+++ class-09/demo/virtual-joins/todo.js
@@ -1,43 +1,68 @@
 'use strict';
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');//pg connective tissue between our app and db objest relation methoud
 
-const URI = 'mongodb://localhost:27017/class-09';
-mongoose.connect(URI);
+
+const URI = 'mongodb://localhost:27017/class-09'; //db name  // enviroment varble
+mongoose.connect(URI); 
 
 // Create 2 Schemas
 const todoSchema = mongoose.Schema({
+  //database blue prient
+  task :{type:string }
   task: { type: String },
-  assignee: { type: String },
+  assignee: { type: String }, //how owen this task
   complete: { type: Boolean }
-}, { toObject: { virtuals: true }, toJSON: { virtuals: true } });
+}, { toObject: { virtuals: true }, toJSON: { virtuals: true } }); //what mongoose till you to do  when you want to make virsuail  (object or virsual oriented)
+/1////////////////////////////////////////////////////////////////////////////////////////
 
-todoSchema.virtual('assigneeDetails', {
-  ref: 'people',
-  localField: 'assignee',
-  foreignField: 'name',
-  justOne: true,
+
+
+//when we say assignee details on our to do schema we'ar referencing the people model  .... and what we;re doing is we're pulling the name field so that we have assignee name attached 
+
+
+todoSchema.virtual('assigneeDetails', { // create connection btween when i have person have task ,name ,assignee..> that called from specific person (person schema)
+  ref: 'people', //what spesific model whan to referance
+  localField: 'assignee', // 
+  foreignField: 'name',// pk ,fk
+  justOne: true, //give me one response
 });
 
+////3///////////////////////////////////////////////////////////////////////
 todoSchema.pre('find', join);
 todoSchema.pre('findOne', join);
 
+
+
+
+
+
+
 function join() {
   try {
-    this.populate('assigneeDetails');
+    this.populate('assigneeDetails'); //this is where we assign a todo item to a specific person
   }
-  catch (e) { console.log('Find Error', e); }
+  catch (e) { console.log('Find Error', e); } //if we cant log an error
 }
+
+///4//////////////////////////////////////////////
+
+
+
+
 
 const personSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String },
-  phone: { type: String },
+  phone: { type: String }, //regular expression 
 });
+//2/////////////////////////////////////////////////////////////////
+
+
 
 // In Mongoose, we need to create classes out of the schemas
-const Todo = mongoose.model('todos', todoSchema);
-const Person = mongoose.model('people', personSchema);
+const Todo = mongoose.model('todos', todoSchema);//add s //blue prient is the schema
+const Person = mongoose.model('people', personSchema); //people collection name
 
 // --------------------------------------------
 
